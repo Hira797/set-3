63a8250e4e8129038d9525d5ab5b300d2799170a
Hira797 <48818522+Hira797@users.noreply.github.com>
2019-08-01 10:37:32 -0700

    Add files via upload

---
+++ set3.js
@@ -0,0 +1,54 @@
+
+// =====q 1========
+// for ( let i = 0; i <= 8; i++){
+
+
+// console.log(i + 1);
+// }
+
+// ======= 2 ===
+// for ( let i = 1; i <= 10; i++){
+//     console.log( 2 * i);
+// }
+
+// //tabe of 2
+// let a = repeat;
+// let b;
+
+// for(let i = 1; i <= 10; i++){
+//     b = a * i;
+
+//     // document.write
+//     console.log( " " + a + " * " + i + " = " , + b + "");
+// }
+
+
+// ===== 4 ======
+
+// let students = [
+//     {
+//         name: "Amna",
+//         hobbies: ["eating", "cooking"]
+//     },
+//     {
+//         name: "Daniyal",
+//         hobbies: ["arts", "shopping"]
+//     },
+//     {
+//         name: "Fahad",
+//         hobbies: ["coding", "cooking"]
+//     },
+//     {
+//         name: "Hajra",
+//         hobbies: ["sleep", "reading"]
+//     }
+// ];
+// for (let i = 0; i < students.length; i++){
+//     if(students[i].name + students[i].hobbies){
+//          console.log( "Hobbies Of " + students[i].name + " " +students[i].hobbies);
+//          console.log(" ");
+//     }
+// }
+
+
+// =======
